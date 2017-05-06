8b69c77461052312434f9317cc7a5218225cc16b
TPeterW <peterwangtao0@hotmail.com>
2017-05-06 05:19:02 -0400

    This is amazing

--- src/com/tpwang/sql/SQLQuery.java
+++ src/com/tpwang/sql/SQLQuery.java
@@ -14,7 +14,7 @@ public class SQLQuery {
 	/***
 	 * What to query
 	 * @param target		Selection target from table
-	 * @return				builder
+	 * @return				query
 	 */
 	public SQLQuery select(String target) {
 		builder.append("SELECT ").append(target).append(' ').toString();
@@ -23,8 +23,22 @@ public class SQLQuery {
 	
 	/***
 	 * What to query
+	 * @param target		Selection targets from table
+	 * @return				query
+	 */
+	public SQLQuery select(String... targets) {
+		builder.append("SELECT ");
+		for (String str : targets) {
+			builder.append(str);
+		}
+		builder.append(' ');
+		return this;
+	}
+	
+	/***
+	 * What to query
 	 * @param target		Selection target from table
-	 * @return				builder
+	 * @return				query
 	 */
 	public SQLQuery select(char target) {
 		return select(Character.toString(target));
@@ -32,8 +46,8 @@ public class SQLQuery {
 	
 	/***
 	 * Where to query
-	 * @param tableName		Selection destination/subquery
-	 * @return				builder
+	 * @param tableName		Selection destination
+	 * @return				query
 	 */
 	public SQLQuery from(String tableName) {
 		builder.append("FROM ").append(tableName).append(' ');
@@ -41,9 +55,29 @@ public class SQLQuery {
 	}
 	
 	/***
+	 * Where to query
+	 * @param subquery		Selection destination (subquery)
+	 * @return				query
+	 */
+	public SQLQuery fromSubquery(String subquery) {
+		builder.append("FROM (").append(subquery).append(") ");
+		return this;
+	}
+	
+	/***
+	 * Where to query
+	 * @param subquery		Selection destination (subquery)
+	 * @return				query
+	 */
+	public SQLQuery fromSubquery(SQLSubquery subquery) {
+		builder.append("FROM (").append(subquery.create()).append(") ");
+		return this;
+	}
+	
+	/***
 	 * How to query
 	 * @param condition		Selection condition (manual or from Attribute)
-	 * @return				builder
+	 * @return				query
 	 */
 	public SQLQuery where(String condition) {
 		builder.append("WHERE ").append(condition).append(' ');
@@ -53,7 +87,7 @@ public class SQLQuery {
 	/***
 	 * How to query
 	 * @param condition		Selection condition (manual or from Attribute)
-	 * @return				builder
+	 * @return				query
 	 */
 	public SQLQuery where(SQLCondition condition) {
 		builder.append("WHERE ").append(condition.create()).append(' ');
--- src/com/tpwang/sql/SQLSubQuery.java
+++ src/com/tpwang/sql/SQLSubQuery.java
@@ -4,47 +4,93 @@ public class SQLSubquery {
 	
 	private SQLQuery query;
 	
+	// whether to rename
 	private boolean hasNewName = false;
 	private String newName = null;
 	
+	// whether a join statement
+	private boolean isJoinStmt = false;
+	private StringBuilder joinBuilder = null;
+	
 	public SQLSubquery() {
 		this.query = new SQLQuery();
+		joinBuilder = new StringBuilder().append(' ');
 	}
 	
 	/***
 	 * What to subquery
 	 * @param target		Selection target from table
-	 * @return				builder
+	 * @return				subquery
 	 */
 	public SQLSubquery select(String target) {
 		query.select(target);
+		joinBuilder.append("SELECT ").append(target).append(' ');
+		return this;
+	}
+	
+	/***
+	 * What to query
+	 * @param target		Selection targets from table
+	 * @return				query
+	 */
+	public SQLSubquery select(String... targets) {
+		query.select(targets);
+		joinBuilder.append("SELECT ");
+		for (String str : targets) {
+			joinBuilder.append(str);
+		}
+		joinBuilder.append(' ');
 		return this;
 	}
 	
 	/***
 	 * What to subquery
 	 * @param target		Selection target from table
-	 * @return				builder
+	 * @return				subquery
 	 */
 	public SQLSubquery select(char target) {
 		query.select(target);
+		joinBuilder.append("SELECT ").append(target).append(' ');
 		return this;
 	}
 	
 	/***
 	 * Where to subquery
 	 * @param tableName		Selection destination/subquery
-	 * @return				builder
+	 * @return				subquery
 	 */
 	public SQLSubquery from(String tableName) {
 		query.from(tableName);
+		joinBuilder.append("FROM ").append(tableName).append(' ');
+		return this;
+	}
+	
+	/***
+	 * Where to query
+	 * @param subquery		Selection destination (subquery)
+	 * @return				query
+	 */
+	public SQLSubquery fromSubquery(String subquery) {
+		query.fromSubquery(subquery);
+		joinBuilder.append("FROM (").append(subquery).append(") ");
+		return this;
+	}
+	
+	/***
+	 * Where to query
+	 * @param subquery		Selection destination (subquery)
+	 * @return				query
+	 */
+	public SQLSubquery fromSubquery(SQLSubquery subquery) {
+		query.fromSubquery(subquery);
+		joinBuilder.append("FROM (").append(subquery.create()).append(") ");
 		return this;
 	}
 	
 	/***
 	 * How to subquery
 	 * @param condition		Selection condition (manual or from Attribute)
-	 * @return				builder
+	 * @return				subquery
 	 */
 	public SQLSubquery where(String condition) {
 		query.where(condition);
@@ -54,10 +100,62 @@ public class SQLSubquery {
 	/***
 	 * How to subquery
 	 * @param condition		Selection condition (manual or from Attribute)
-	 * @return				builder
+	 * @return				subquery
 	 */
 	public SQLSubquery where(SQLCondition condition) {
 		query.where(condition);
+		return this;
+	}
+	
+	/***
+	 * What to join
+	 * @param tableName		Join table left
+	 * @return				subquery
+	 */
+	public SQLSubquery whereTable(String tableName) {
+		joinBuilder.append(tableName).append(' ');
+		return this;
+	}
+	
+	/***
+	 * What to join
+	 * @param tableName		Join table left
+	 * @return				subquery
+	 */
+	public SQLSubquery whereTable(SQLSubquery tableSubquery) {
+		joinBuilder.append("( ").append(tableSubquery.create()).append(") ");
+		return this;
+	}
+	
+	/***
+	 * What to join to
+	 * @param tableName		Join table right
+	 * @return				subquery
+	 */
+	public SQLSubquery joinsTable(String targetTableName) {
+		isJoinStmt = true;
+		joinBuilder.append("JOIN ").append(targetTableName).append(' ');
+		return this;
+	}
+	
+	/***
+	 * What to join to
+	 * @param tableName		Join table right
+	 * @return				subquery
+	 */
+	public SQLSubquery joinsTable(SQLSubquery targetTableSubquery) {
+		isJoinStmt = true;
+		joinBuilder.append(targetTableSubquery.create()).append(' ');
+		return this;
+	}
+	
+	/***
+	 * What to join on
+	 * @param attribute		Join attribute
+	 * @return				subquery
+	 */
+	public SQLSubquery onAttribute(String tableName, String attributeName) {
+		joinBuilder.append("ON ").append('`').append(tableName).append('.').append(attributeName).append("` ");
 		return this;
 	}
 	
@@ -95,7 +193,9 @@ public class SQLSubquery {
 	 * Return the final subquery
 	 */
 	public String toString() {
-		return (new StringBuilder())
+		return isJoinStmt 
+				? joinBuilder.toString()
+				: (new StringBuilder())
 				.append(query.create())
 				.append(hasNewName ? "as " + newName : "")
 				.toString();
